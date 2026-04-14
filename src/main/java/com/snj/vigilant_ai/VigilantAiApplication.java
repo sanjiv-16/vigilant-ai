4bf479dd96bb087618aa0680f847df7efd507906
Kumar Sanjeevi MT-SWDC-IN <Sanjeevi.Kumar@mt.com>
2026-04-14 10:31:33 +0530

    Initial commit

---
+++ src/main/java/com/snj/vigilant_ai/VigilantAiApplication.java
@@ -0,0 +1,13 @@
+package com.snj.vigilant_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VigilantAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VigilantAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snj/vigilant_ai/VigilantAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snj.vigilant_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VigilantAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
